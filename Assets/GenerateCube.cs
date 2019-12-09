e73d6adc4dfe55a296381de2d65b1416d7590e0e
Joshua Lewis <josh@jlewisgames.co.uk>
2019-12-09 15:41:02 +0000

    Add files via upload

---
+++ Assets/GenerateCube.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class GenerateCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "GeneratedCube";
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Vector3[] vertices =
+        {
+            new Vector3 (0, 0, 0),
+            new Vector3 (1, 0, 0),
+            new Vector3 (1, 1, 0),
+            new Vector3 (0, 1, 0),
+            new Vector3 (0, 1, 1),
+            new Vector3 (1, 1, 1),
+            new Vector3 (1, 0, 1),
+            new Vector3 (0, 0, 1),
+        };
+
+        Vector3[] normals =
+        {
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f)
+        };
+
+        int[] triangles = 
+        { 
+            0, 2, 1,
+            0, 3, 2,
+            2, 3, 4,
+            2, 4, 5,
+            1, 2, 5,
+            1, 5, 6,
+            0, 7, 4,
+            0, 4, 3,
+            5, 4, 7,
+            5, 7, 6,
+            0, 6, 7,
+            0, 1, 6
+        
+        };
+
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GenerateSphere.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class GenerateSphere : MonoBehaviour
+{
+    public int subdivisions = 0;
+    public float radius = 1f;
+
+    private void Start()
+    {
+        GetComponent<MeshFilter>().mesh = SphereGenerator.Create(subdivisions, radius);
+    }
+}
